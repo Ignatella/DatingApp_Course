2a5ec64fef255e0e905335d6d6dd045d4d03dfbf
Ignatella <ignatellasecond@gmail.com>
2020-06-23 18:21:56 +0200

    Add SetTheMainPhoto functionality in .API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,9 @@ namespace DatingApp.API.Data
         
         public void Delete<T>(T entity) where T : class =>
             context.Remove(entity);
+
+        public async Task<Photo> GetMainPhotoForUser(int userId) =>
+            await context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
 
         public async Task<Photo> GetPhoto(int id) =>
           await  context.Photos.FirstOrDefaultAsync(photo => photo.Id == id);
