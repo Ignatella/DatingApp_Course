49c125e1046bf5df9002bc216ac46fb2352ad0b6
Ignatella <ignatellasecond@gmail.com>
2020-07-06 18:25:30 +0300

    Add user pagination in -SPA

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_modules/user';
+import { PaginatedResult } from '../_modules/pagination';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,23 +13,48 @@ export class UserService {
   constructor(private http: HttpClient) {}
   baseUrl = environment.apiUrl;
 
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl + 'users');
+  getUsers(page?, itemsPerPage?): Observable<PaginatedResult<User[]>> {
+    const paginatedResult: PaginatedResult<User[]> = new PaginatedResult();
+
+    let params = new HttpParams();
+    if (page != null && itemsPerPage != null) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    return this.http
+      .get<User[]>(this.baseUrl + 'users', { observe: 'response', params })
+        .pipe(
+          map((response) => {
+            paginatedResult.result = response.body;
+            if (response.headers.get('Pagination') != null) {
+              paginatedResult.pagination = JSON.parse(
+                response.headers.get('Pagination')
+              );
+              return paginatedResult;
+            }
+          })
+      );
   }
 
   getUser(id): Observable<User> {
     return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
 
-  updateUser(id: number, user: User){
-     return this.http.put(this.baseUrl + 'users/' + id, user);
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + 'users/' + id, user);
   }
 
-  setMainPhoto(userId: number, photoId: number){
-    return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + photoId + '/setMain', {});
+  setMainPhoto(userId: number, photoId: number) {
+    return this.http.post(
+      this.baseUrl + 'users/' + userId + '/photos/' + photoId + '/setMain',
+      {}
+    );
   }
 
   deletePhoto(userId: number, photoId: number) {
-    return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + photoId);
+    return this.http.delete(
+      this.baseUrl + 'users/' + userId + '/photos/' + photoId
+    );
   }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { TimeagoModule } from 'ngx-timeago';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -58,6 +59,7 @@ export function tokenGetter() {
     NgxGalleryModule,
     ReactiveFormsModule,
     FileUploadModule,
+    PaginationModule.forRoot(),
     TimeagoModule.forRoot(),
     BsDropdownModule.forRoot(),
     BsDatepickerModule.forRoot(),
